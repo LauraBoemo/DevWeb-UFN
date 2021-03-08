52d475efbd9ed7d74ea1bde29411789e2f95f227
Laura Boemo <71987461+LauraBoemo@users.noreply.github.com>
2021-03-08 19:53:27 -0300

    commi

--- Aula01/Aula01.php
+++ Aula01/Aula01.php
@@ -4,5 +4,12 @@
 <body>
 <P><center><h1>Olá Mundo!</h1></center></P>
 Essa é nossa segunda aula e estamos usando o <a href="https://www.apachefriends.org">Xamp!</a> ...mentira, eu estou com Wamp... hehe
+<?php
+  $x = 0;
+  while($x<10){
+    echo "<p>Conteúdo de $x";
+    $x++;
+  }
+?>
 </body>
 </html>
