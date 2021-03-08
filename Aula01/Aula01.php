e1b68aa5a0e790ca1ecfdc80c632c8f95ecff2c3
Laura Boemo <71987461+LauraBoemo@users.noreply.github.com>
2021-03-08 20:00:43 -0300

    FinalAula01

--- Aula01/Aula01.php
+++ Aula01/Aula01.php
@@ -3,13 +3,19 @@
 <head><title>Aula programação</title></head>
 <body>
 <P><center><h1>Olá Mundo!</h1></center></P>
+<center>
 Essa é nossa segunda aula e estamos usando o <a href="https://www.apachefriends.org">Xamp!</a> ...mentira, eu estou com Wamp... hehe
 <?php
   $x = 0;
   while($x<10){
-    echo "<p>Conteúdo de $x";
+    if($x%2==0){
+      echo"<p><font color=\"red\">Conteúdo de $x</p></font>";
+    }else{
+      echo "<p>Conteúdo de $x";
+    }
     $x++;
   }
 ?>
+</center>
 </body>
 </html>
