62234be991af2ca9682f2cc363367868815c85e0
Laura Boemo <laura.boemo@mblabs.com.br>
2021-05-09 14:53:54 -0300

    a

--- Aula06/exercicio.js
+++ Aula06/exercicio.js
@@ -1,10 +1,44 @@
-function teste() {
-  var numero = document.forms["PoI"]["numero"].value
-  var out = document.forms["PoI"]["numero"].value
-  var paroimp = numero % 2 
-    if(paroimp = 1){
-      out.innerHTML = "Este é um número impar."
-    }else{
-      out.innerHTML = "Este é um número par."
-    }
+function calc(){
+  var op, val1, val2, result, aux;
+
+  op = document.getElementById("op").value
+  var aux = document.getElementById("val1").value
+  val1 = parseInt(aux)
+  var aux2 = document.getElementById("val2").value
+  val2 = parseInt(aux2)
+
+  switch (op) {
+      case "1":
+          result = val1 + val2;
+          break;
+      case "2":
+          if(val1 < val2){
+              aux = val1;
+              val1 = val2;
+              val2 = aux;
+              result = val1 - val2;
+          }else{
+              result = val1 - val2;
+          }
+          break;
+      case "3":
+          result = val2 * val2;
+          break;
+      case "4":
+          if(val1 < val2){
+              aux = val1;
+              val1 = val2;
+              val2 = aux;
+              result = val1 / val2;
+          }else{
+              result = val1 / val2;
+          }
+          break;
+      default:
+          result = "Operador escolhido inválido.";
+          break;
+  }
+
+  var localResultado = document.getElementById("resultado");
+  localResultado.innerHTML = "<h3>O resultado é: "+result+"</h3>";
 }
